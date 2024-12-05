f227edfd682be97ff09b062ab06d695218a1d66c
José Eliel Camargo Molina <j.camargo-molina@imperial.ac.uk>
2024-12-05 19:24:10 +0100

    added readthedocs file

--- doc/conf.py
+++ doc/conf.py
@@ -20,8 +20,7 @@ release = '1.0.0'
 
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
-              'sphinx.ext.napoleon',
-              'myst_parser']
+              'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
